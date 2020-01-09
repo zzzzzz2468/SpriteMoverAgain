5fd4a0c739f2672576ae657e37a05e42146a26a6
Elijah <enutter24@gmail.com>
2020-01-09 01:12:11 -0700

    Changes in script

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -11,15 +11,16 @@ public class Movement : MonoBehaviour
 
     void Start()
     {
+        //getting the location of the player on start to return if space pressed
         startPos = player.transform.position;
     }
 
     void Update()
     {
-        if (canMove && Input.GetKeyDown(KeyCode.P))
+        //enabling and disabling movement in game
+        if (canMove == true && Input.GetKeyDown(KeyCode.P))
             canMove = false;
-
-        if (!canMove && Input.GetKeyDown(KeyCode.P))
+        else if (canMove == false && Input.GetKeyDown(KeyCode.P))
             canMove = true;
 
         //Movement
@@ -34,7 +35,6 @@ public class Movement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
             player.transform.position = startPos;
 
-
         //Setting the player inactive
         if (Input.GetKeyDown(KeyCode.Q))
         {
@@ -47,31 +47,30 @@ public class Movement : MonoBehaviour
             }
                 
         }
-            
-
 
         //Input For exiting the game
         if (Input.GetKeyDown(KeyCode.Escape))
             Exit();
 
+        //Limiting how far the player can move from center
         if (Input.GetKey(KeyCode.LeftShift))
         {
             if (player.transform.position.x >= 5 || player.transform.position.x <= -5)
-            {
                 canMove = false;
-                Debug.Log(canMove);
-                
-            }
             else if (player.transform.position.y >= 5 || player.transform.position.y <= -5)
-            {
                 canMove = false;
-                Debug.Log(canMove);
-            }
-            else
-            {
-                canMove = true;
-            }
         }
+
+        //allowing the player to move freely again
+        else if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            if (player.transform.position.x >= 5 || player.transform.position.x <= -5)
+                canMove = true;
+            else if (player.transform.position.y >= 5 || player.transform.position.y <= -5)
+                canMove = true;
+        }
+
+        gridMovement();
     }
 
 
@@ -81,6 +80,7 @@ public class Movement : MonoBehaviour
         Application.Quit();
     }
 
+    //causing the player to be able to move
     private void playerMovement(float hor, float vert)
     {    
         if(canMove == true)
@@ -91,5 +91,40 @@ public class Movement : MonoBehaviour
                 transform.position += transform.right * hor * speed * Time.deltaTime;
         }
 
+    }
+
+    private void gridMovement()
+    {
+        Vector3 temp = new Vector3(7.0f, 2.0f, 0);
+        if (Input.GetKey(KeyCode.Alpha1))
+            player.transform.position = temp;
+        temp = new Vector3(4.0f, -1.0f, 0);
+        if (Input.GetKey(KeyCode.Alpha2))
+            player.transform.position = temp;
+        temp = new Vector3(-2.0f, 5.0f, 0);
+        if (Input.GetKey(KeyCode.Alpha3))
+            player.transform.position = temp;
+        temp = new Vector3(-2.0f, -4.0f, 0);
+        if (Input.GetKey(KeyCode.Alpha4))
+            player.transform.position = temp;
+        temp = new Vector3(3.0f, -3.0f, 0);
+        if (Input.GetKey(KeyCode.Alpha5))
+            player.transform.position = temp;
+        temp = new Vector3(7.0f, 3.0f, 0);
+        if (Input.GetKey(KeyCode.Alpha6))
+            player.transform.position = temp;
+        temp = new Vector3(1.0f, 1.0f, 0);
+        if (Input.GetKey(KeyCode.Alpha7))
+            player.transform.position = temp;
+        temp = new Vector3(-2.0f, -3.0f, 0);
+        if (Input.GetKey(KeyCode.Alpha8))
+            player.transform.position = temp;
+        temp = new Vector3(-7.0f, 2.0f, 0);
+        if (Input.GetKey(KeyCode.Alpha9))
+            player.transform.position = temp;
+        temp = new Vector3(-2.0f, -5.0f, 0);
+        if (Input.GetKey(KeyCode.Alpha0))
+            player.transform.position = temp;
+        temp = new Vector3(-6.0f, -1.0f, 0);
     }
 }
